c232f56b5b1335812b8027540f34daa408a951e3
perugo <34999008+perugo@users.noreply.github.com>
2025-06-22 11:28:48 +0900

    タイムゾーン tokyo, 言語デフォルト ja

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,13 @@ module RailsReact
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = %i(ja)
+    config.i18n.default_locale = :ja
+
+    config.autoload_paths += Dir[Rails.root.join('lib')]
+    config.autoload_paths += Dir[Rails.root.join('lib', '**')]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
